effd45ea4824f919293304942455c7dd7479f428
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-07-20 15:55:57 -0400

    added component screen and list screen

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ListScreen = () => {
+
+  return 
+    <Text> List Screen</Text>
+  
+};
+
+const styles = StyleSheet.create({});
+
+export default ListScreen;
