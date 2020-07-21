df8e6b6738e81cc8609fa95bf0511b532cb00106
Cassandra Lin <56838251+cassandralin@users.noreply.github.com>
2020-07-21 13:34:46 -0400

    added <Button />

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Cassandra's App</Text>;
+  return (
+    <View>
+    <Text style={styles.text}>Cassandra's App</Text>
+    <Button 
+      onPress={() => console.log('Button pressed')}
+      title="Go to the components demo" 
+    />
+    </View>
+    );
 };
 
 const styles = StyleSheet.create({
